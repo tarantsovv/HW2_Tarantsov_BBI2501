63ad39d7b0d60fc6b758ccf39d2ae354691fd778
Egor Tarantsov <benzogangegorka@gmail.com>
2026-06-03 21:17:44 +0300

    написал класс Ingredient, полностью соблюдя требования

---
+++ ingredient.py
@@ -0,0 +1,29 @@
+class Ingredient: 
+    def __init__(self, name = "", quantity_default = 0.0, unit = ""):
+        self.name = name
+        self._quantity = quantity_default
+        self.unit = unit
+
+    @property
+    def quantity(self):
+        return self._quantity
+
+    @quantity.setter
+    def quantity(self, value):
+        if value <= 0:
+            raise ValueError("Количество должно быть положительным")
+        else:
+            self._quantity = float(value)
+    
+    def __str__(self):
+        return f"{self.name}: {self.quantity} {self.unit}"
+
+    def __repr__(self):
+        return f"Ingredient('{self.name}', {self.quantity}, '{self.unit}')"
+
+    def __eq__(self, other):
+        if (self.name == other.name and self.unit == other.unit):
+            return True
+        else:
+            return False
+    
