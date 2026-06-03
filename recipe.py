3b4881a2886ce21a973d24d7cffa87f5f94fe26d
Egor Tarantsov <benzogangegorka@gmail.com>
2026-06-04 00:02:01 +0300

    Создал класс Recipe в файле recipe.py. В конструкторе инициализируются название рецепта(str) и список ингредиентов(обычный список). Добавил метод add_ingredient, позволяющий добавлять ингредиенты с проверкой на уникальность во избежание создания дубликатов. Добавил статический метод is_valid_ratio, принимающий аргумент ratio и проверяющий, является ли ratio положительным числом. Проверку реализовал через try/except/else (в try пробуем создать float версию объекта, если не получается - значит ratio точно не число и возвращаем False в except, иначе в else проверяем на положительность). Добавил метод scale(ratio:float), увеличивающий в рецепте количество каждого ингредиента в ratio раз. Добавил метод __len__, возвращающий количество различных ингредиентов, и  метод __str__ возвращающий название блюда и рецепт в читаемом виде.

---
+++ recipe.py
@@ -0,0 +1,42 @@
+from ingredient import Ingredient
+class Recipe:
+    def __init__(self, title = "", ingredients = None):
+        self.title = title
+        self.ingredients = ingredients if ingredients is not None else []
+    
+    def add_ingredient(self, ingredient: Ingredient):
+        found = False
+        for ingredient_val in self.ingredients:
+            if ingredient_val == ingredient:
+                ingredient_val.quantity = ingredient_val.quantity + ingredient.quantity
+                found = True
+                break
+        if not found:
+            self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        try:
+            ratio = float(ratio)
+        except Exception:
+            return False
+        else: 
+            if ratio > 0:
+                return True
+            else:
+                return False
+    
+    def scale(self, ratio: float):
+        new_recipe = Recipe(self.title)
+        for val in self.ingredients:
+            new_quantity = val.quantity * ratio
+            new_ingredient = Ingredient(val.name, new_quantity, val.unit)
+            new_recipe.add_ingredient(new_ingredient)
+        return new_recipe
+    
+    def __len__(self):
+        return len(self.ingredients)
+    
+    def __str__(self):
+        result_ingredients_list = "\n".join(f" - {ingredient}" for ingredient in self.ingredients)
+        return f"Блюдо: {self.title} состоит из следующих элементов: \n {result_ingredients_list}"
