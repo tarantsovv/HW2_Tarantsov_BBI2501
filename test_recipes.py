3e85abd3f332c891e92e3d16a286375bc976d834
Egor Tarantsov <benzogangegorka@gmail.com>
2026-06-04 01:35:24 +0300

    Добавил тесты для класса Recipe в файл test_recipes.py. Создал test_recipe_creation, который проверяет правильное создание пустого рецепта и инициализацию его имени. Добавил test_recipe_add_ingredient, проверяющий что новые элементы успешно добавляются в список, а дубликаты ингредиентов не появляются, а их количество корректно складывается. Также метод test_recipe_scale, проверяющий что метод scale возвращает  новый объект с измененным в ratio раз количеством ингредиентов, не изменяя при этом исходный рецепт.

--- test_recipes.py
+++ test_recipes.py
@@ -24,3 +24,31 @@ def test_ingredient_eq():
     assert ingro1 == ingro2
     assert ingro1 != ingro3
     assert ingro2 != ingro4
+
+
+def test_recipe_creation():
+    recipe = Recipe("Пицца Маргарита")
+    assert recipe.title == "Пицца Маргарита"
+    assert recipe.ingredients == []
+
+def test_recipe_add_ingredient():
+    recipe = Recipe("Тесто")
+    ingro1 = Ingredient("Мука", 200.0, "г")
+    ingro2 = Ingredient("Мука", 300.0, "г")
+    ingro3 = Ingredient("Вода", 100.0, "мл")
+    
+    recipe.add_ingredient(ingro1)
+    assert len(recipe) == 1
+    recipe.add_ingredient(ingro2)
+    assert len(recipe) == 1
+    assert recipe.ingredients[0].quantity == 500.0
+    recipe.add_ingredient(ingro3)
+    assert len(recipe) == 2
+
+def test_recipe_scale():
+    recipe = Recipe("Соус")
+    recipe.add_ingredient(Ingredient("Томаты", 2.0, "шт"))
+    scaled = recipe.scale(3.0)
+    assert scaled is not recipe
+    assert scaled.ingredients[0].quantity == 6.0
+    assert recipe.ingredients[0].quantity == 2.0
